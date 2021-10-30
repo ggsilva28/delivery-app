a54a6ca0c1da3fd2c38d13dd90584270bdf15838
ggsilva <gagsilva28@gmail.com>
2021-10-30 12:46:06 -0300

    Here goes nothing

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'pedi-facil',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
